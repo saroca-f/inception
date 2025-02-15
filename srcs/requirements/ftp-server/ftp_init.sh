7864624b2d97aef3e94d9f79d6d38e8805f3b8b1
Sergio Aroca <saroca1993@gmail.com>
2025-02-15 19:45:45 +0100

    ftp-server add

---
+++ srcs/requirements/ftp-server/ftp_init.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+if [ ! -f "/etc/vsftpd/vsftpd.conf.bak" ]; then
+
+    mkdir -p /var/www/html
+
+    cp /etc/vsftpd/vsftpd.conf /etc/vsftpd/vsftpd.conf.bak
+    mv /tmp/vsftpd.conf /etc/vsftpd/vsftpd.conf
+
+    sed -i "s|listen=NO|listen=YES|g" /etc/vsftpd.conf
+    sed -i "s|anonymous_enable=NO|anonymous_enable=YES|g" /etc/vsftpd.conf
+    sed -i "s|#write_enable=YES|write_enable=YES|g" /etc/vsftpd.conf
+    sed -i "s|#ftpd_banner=Welcome to blah FTP service.|ftpd_banner=Welcome to Inception FTP server!|g" /etc/vsftpd.conf
+    sed -i "s|#chroot_local_user=YES|chroot_local_user=YES|g" /etc/vsftpd.conf
+    sed -i "s|#chroot_list_enable=YES|chroot_list_enable=YES" /etc/vsftpd.conf
+    sed -i "s|secure_chroot_dir=/var/run/vsftpd/empty|#secure_chroot_dir=/var/run/vsftpd/empty|g" /etc/vsftpd.conf
+
+    echo "" >> /etc/vsftpd.conf
+
+    echo "chroot_local_user=YES" >> /etc/vsftpd.conf
+    echo "allow_writeable_chroot=YES" >> /etc/vsftpd.conf
+    echo "user_sub_token=$FTP_USER" >> /etc/vsftpd.conf
+    echo "local_root=/var/www/html" >> /etc/vsftpd.conf
+    echo "listen_port=21" >> /etc/vsftpd.conf
+    echo "listen_address=0.0.0.0" >> /etc/vsftpd.conf
+    echo "seccomp_sandbox=NO" >> /etc/vsftpd.conf
+    echo "pasv_enable=YES" >> /etc/vsftpd.conf
+    echo "pasv_max_port=21100" >> /etc/vsftpd.conf
+    echo "pasv_max_port=21110" >> /etc/vsftpd.conf
+    echo "userlist_enable=YES" >> /etc/vsftpd.conf
+    echo "userlist_file=/etc/vsftpd.userlist" >> /etc/vsftpd.conf
+    echo "userlist_deny=NO" >> /etc/vsftpd.conf
+
+    # Create ftp_server user and asing a password
+    useradd -m -d /var/www "$FTP_USER" && echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
+
+    # Add ftp_user user to the www-data group with security
+    usermod -aG www-data "$FTP_USER"
+
+    chown -R $FTP_USER:$FTP_USER /var/www/html
+
+    echo "$FTP_USER" | tee -a /etc/vsftpd.userlist > /dev/null
+
+    chown -R www-data /var/www
+
+    chmod -R 777 /var/www
+fi
+
+echo "FTP started on :21"
+/usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
