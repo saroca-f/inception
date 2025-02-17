35b5b9b618cc3b749807560c0f968e41d523a277
Sergio Aroca <saroca1993@gmail.com>
2025-02-17 19:16:29 +0100

    ftp-server test to check 2

--- srcs/requirements/ftp-server/ftp_init.sh
+++ srcs/requirements/ftp-server/ftp_init.sh
@@ -2,34 +2,35 @@
 
 if [ ! -f "/etc/vsftpd/vsftpd.conf.bak" ]; then
 
+    mkdir -p /etc/vsftpd
     mkdir -p /var/www/html
     mkdir -p /var/run/vsftpd/empty
 
     cp /etc/vsftpd/vsftpd.conf /etc/vsftpd/vsftpd.conf.bak
 
-    sed -i "s|listen=NO|listen=YES|g" /etc/vsftpd.conf
-    sed -i "s|anonymous_enable=NO|anonymous_enable=YES|g" /etc/vsftpd.conf
-    sed -i "s|#write_enable=YES|write_enable=YES|g" /etc/vsftpd.conf
-    sed -i "s|#ftpd_banner=Welcome to blah FTP service.|ftpd_banner=Welcome to Inception FTP server!|g" /etc/vsftpd.conf
-    sed -i "s|#chroot_local_user=YES|chroot_local_user=YES|g" /etc/vsftpd.conf
-    sed -i "s|#chroot_list_enable=YES|chroot_list_enable=YES" /etc/vsftpd.conf
-    sed -i "s|secure_chroot_dir=/var/run/vsftpd/empty|#secure_chroot_dir=/var/run/vsftpd/empty|g" /etc/vsftpd.conf
+    sed -i "s|listen=NO|listen=YES|g" /etc/vsftpd/vsftpd.conf
+    sed -i "s|anonymous_enable=NO|anonymous_enable=YES|g" /etc/vsftpd/vsftpd.conf
+    sed -i "s|#write_enable=YES|write_enable=YES|g" /etc/vsftpd/vsftpd.conf
+    sed -i "s|#ftpd_banner=Welcome to blah FTP service.|ftpd_banner=Welcome to Inception FTP server!|g" /etc/vsftpd/vsftpd.conf
+    sed -i "s|#chroot_local_user=YES|chroot_local_user=YES|g" /etc/vsftpd/vsftpd.conf
+    sed -i "s|#chroot_list_enable=YES|chroot_list_enable=YES" /etc/vsftpd/vsftpd.conf
+    sed -i "s|secure_chroot_dir=/var/run/vsftpd/empty|#secure_chroot_dir=/var/run/vsftpd/empty|g" /etc/vsftpd/vsftpd.conf
 
-    echo "" >> /etc/vsftpd.conf
+    echo "" >> /etc/vsftpd/vsftpd.conf
 
-    echo "chroot_local_user=YES" >> /etc/vsftpd.conf
-    echo "allow_writeable_chroot=YES" >> /etc/vsftpd.conf
-    echo "user_sub_token=$FTP_USER" >> /etc/vsftpd.conf
-    echo "local_root=/var/www/html" >> /etc/vsftpd.conf
-    echo "listen_port=21" >> /etc/vsftpd.conf
-    echo "listen_address=0.0.0.0" >> /etc/vsftpd.conf
-    echo "seccomp_sandbox=NO" >> /etc/vsftpd.conf
-    echo "pasv_enable=YES" >> /etc/vsftpd.conf
-    echo "pasv_max_port=21100" >> /etc/vsftpd.conf
-    echo "pasv_max_port=21110" >> /etc/vsftpd.conf
-    echo "userlist_enable=YES" >> /etc/vsftpd.conf
-    echo "userlist_file=/etc/vsftpd.userlist" >> /etc/vsftpd.conf
-    echo "userlist_deny=NO" >> /etc/vsftpd.conf
+    echo "chroot_local_user=YES" >> /etc/vsftpd/vsftpd.conf
+    echo "allow_writeable_chroot=YES" >> /etc/vsftpd/vsftpd.conf
+    echo "user_sub_token=$FTP_USER" >> /etc/vsftpd/vsftpd.conf
+    echo "local_root=/var/www/html" >> /etc/vsftpd/vsftpd.conf
+    echo "listen_port=21" >> /etc/vsftpd/vsftpd.conf
+    echo "listen_address=0.0.0.0" >> /etc/vsftpd/vsftpd.conf
+    echo "seccomp_sandbox=NO" >> /etc/vsftpd/vsftpd.conf
+    echo "pasv_enable=YES" >> /etc/vsftpd/vsftpd.conf
+    echo "pasv_max_port=21100" >> /etc/vsftpd/vsftpd.conf
+    echo "pasv_max_port=21110" >> /etc/vsftpd/vsftpd.conf
+    echo "userlist_enable=YES" >> /etc/vsftpd/vsftpd.conf
+    echo "userlist_file=/etc/vsftpd.userlist" >> /etc/vsftpd/vsftpd.conf
+    echo "userlist_deny=NO" >> /etc/vsftpd/vsftpd.conf
 
     useradd -m -d /var/www "$FTP_USER" && echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
 
@@ -45,4 +46,4 @@ if [ ! -f "/etc/vsftpd/vsftpd.conf.bak" ]; then
 fi
 
 echo "FTP started on :21"
-/usr/sbin/vsftpd /etc/vsftpd.conf
+/usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
