5145c91c25b22c1096218be82feb0e407a7262f6
Sergio Aroca <saroca1993@gmail.com>
2025-02-20 12:40:58 +0100

    Admirer test 2

--- srcs/requirements/adminer/tools/init_admirer.sh
+++ srcs/requirements/adminer/tools/init_admirer.sh
@@ -2,6 +2,4 @@
 
 VERSION=$(php -r "echo PHP_VESION_MAYOR . '.' . PHP_VERSION_MINOR;")
 
-cd /etc/php/$VERSION/fpm/pool.d
-
 sed -i "s|listen = /run/php/php7.4-fpm.sock|listen = 9000|g" /etc/php/$VERSION/fpm/pool.d/www.conf
