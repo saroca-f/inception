c24b4514a48c440d83c8950b346bda0254794e5e
Sergio Aroca <saroca1993@gmail.com>
2025-02-20 15:47:19 +0100

    adminer test

--- srcs/requirements/adminer/tools/init_admirer.sh
+++ srcs/requirements/adminer/tools/init_admirer.sh
@@ -2,10 +2,12 @@
 
 VERSION=$(php -r "echo PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION;")
 
-mkdir /prueba
+mkdir -p /run/php
 
-chmod 777 /etc/php/$VERSION/fpm/pool.d/www.conf
+mv /etc/php/$VERSION/fpm/pool.d/www.conf /etc/php/$VERSION/fpm/pool.d/test
 
-sed -i "s|listen = /run/php/php7.4-fpm.sock|listen = 9000|g" /etc/php/$VERSION/fpm/pool.d/www.conf
+sed -i "s|listen = /run/php/php7.4-fpm.sock|listen = 9000|g" /etc/php/$VERSION/fpm/pool.d/test
+
+mv /etc/php/$VERSION/fpm/pool.d/testo /etc/php/$VERSION/fpm/pool.d/www.conf 
 
 php-fpm$VERSION -F
